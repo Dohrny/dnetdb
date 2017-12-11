e2232ecb24c70408407193731c9c4fafa3a0fc42
Adam <Dohrny@users.noreply.github.com>
2017-12-10 20:55:21 -0600

    it works. just need server-side validation now

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var mysql = require('mysql')
 
 app.set('view engine', 'pug')
 
-//connect to database
+//connect to database. yes tis a test passowrd :)
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -28,13 +28,34 @@ app.use(bodyParser.json()) //bodyparser use json data
 app.use('/public', express.static(path.join(__dirname + '/public')));
 app.use('/views', express.static(path.join(__dirname, 'views')))
 
+
 //to render .pug files
-app.use('/', function(req, res) {
-    res.render('showdb')
+app.use('/index', function(req, res) {
+    res.render('index')
+})
+app.use('/showdb', function(req, res) {
+    var personList = []
+
+    var sql = 'SELECT * FROM registration_table'
+    connection.query(sql, function(err, rows) {
+        if (err) throw err
+        
+        for (var i in rows) {
+            var row = rows[i].first_name
+            var person = {
+                'first_name':rows[i].first_name,
+                'last_name':rows[i].last_name,
+                'email':rows[i].email,
+                'role':rows[i].role
+            }
+            personList.push(person)
+        }
+        res.render('showdb', {'personList': personList})
+    })
 })
 
 //get data to be put in db and put that mofo in db
-app.post('/views', function (req, res) {
+app.post('/', function (req, res) {
     console.log(req.body.forename)
 
     console.log(connection.state)
@@ -49,25 +70,8 @@ app.post('/views', function (req, res) {
       if (err) throw err
     console.log(connection.state)
     })
-    res.sendFile(__dirname + '/views/index.html')
+    res.render('index')
 })
-
-app.get('/showdb', function(req, res) {
-    console.log("get showdb")
-    var sql = 'SELECT * FROM registration_table'
-    connection.query(sql, function(err, rows) {
-        if (err) throw err
-        res.json(rows)
-        //console.log("")
-
-        for (var i = 0; i < rows.length; i++) {
-            var row = rows[i]
-            console.log(row.first_name)
-            console.log("test")
-        }
-    })
-})
-
 
 var server = app.listen(8081, function () {
     var host = server.address().address
